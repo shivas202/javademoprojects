e8e4c6221d8bb4d86f34bbd33209b4361653c7b6
Shiva <shivas202@gmail.com>
2022-06-29 17:44:30 +0530

    HttpSession

---
+++ SqServlet with HttpSession.java
@@ -0,0 +1,26 @@
+package com.demoservlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class SqServlet extends HttpServlet 
+{
+	
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException 
+	{
+       HttpSession session = req.getSession();
+       
+		int k = (int) session.getAttribute("k");
+		
+		k = k*k;
+		
+		 PrintWriter out = res.getWriter();
+	     out.println("sq is " + k);
+		System.out.println("Sq Called");
+	}
+}
